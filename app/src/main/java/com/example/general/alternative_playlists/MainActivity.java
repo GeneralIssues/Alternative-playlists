1168933fdb66bc89b80b387ac80d704007397b43
Fred <felmqu14@student.aau.dk>
2015-05-06 22:48:52 +0200

    Added the beginnings of a transition animation (needs big correction) Changed images and how the system loads them Changed text and how the system loads them

--- app/src/main/java/com/example/general/alternative_playlists/MainActivity.java
+++ app/src/main/java/com/example/general/alternative_playlists/MainActivity.java
@@ -1,31 +1,26 @@
 package com.example.general.alternative_playlists;
 
-/**
- * Created by General on 28/04/2015.
- */
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v4.view.GestureDetectorCompat;
 import android.view.GestureDetector;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity implements GestureDetector.OnGestureListener{
 
-    float x1,x2;
     private GestureDetectorCompat mDetector;
-
+    Artist Musk = new Artist(1, "Musk", new String[]{"Fixing the server", "Cookie is dead", "No More Holt"});
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -67,30 +62,86 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        ImageView square = (ImageView)this.findViewById(R.id.imageView);
-        ImageView circle = (ImageView)this.findViewById(R.id.imageView2);
+        final ImageView square = (ImageView)this.findViewById(R.id.imageView);
+        //ImageView circle = (ImageView)this.findViewById(R.id.imageView2);
         TextView genreText = (TextView)this.findViewById(R.id.textView);
-        float sensitvity = 40;
+        TextView songName = (TextView)this.findViewById(R.id.textView2);
+        String[] genres = {"Rock","Pop","Jazz","Wub-Wub"};
+        int genreID = 0;
+        int songID = 0;
+        float sensitivity = 40;
+
+        class MyAnimationListener implements Animation.AnimationListener {
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                square.clearAnimation();
+                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(square.getWidth(), square.getHeight());
+                lp.setMargins(0, 300, 0, 0);
+                square.setLayoutParams(lp);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+        }
+
         //WORKING ON IF e1.getX() >= square.getX() && e1.getX() <= (square.getX() * 2) && e1.getY() >= square.getY() && e1.getY() <= (square.getY() * 2)
     if(e1.getY() <= 400) {
-        if ((e1.getX() - e2.getX()) > sensitvity) {
+        if ((e1.getX() - e2.getX()) > sensitivity) {
             Toast.makeText(this, "Left to Right Swap Performed", Toast.LENGTH_LONG).show();
+            //square.setX(square.getX() - 400);
 
-            square.setX(square.getX() - 300);
-        } else if ((e2.getX() - e1.getX()) > sensitvity) {
+            TranslateAnimation animation = new TranslateAnimation(0, 300, 0, 0);
+            animation.setDuration(1000);
+            animation.setFillAfter(false);
+            animation.setAnimationListener(new MyAnimationListener());
+
+            square.startAnimation(animation);
+
+            songID ++;
+            square.setImageDrawable(getResources().getDrawable(R.drawable.song1));
+            songName.setText(Musk._songs[songID]);
+
+        } else if ((e2.getX() - e1.getX()) > sensitivity) {
             Toast.makeText(this, "Right to Left Swap Performed", Toast.LENGTH_LONG).show();
-            square.setX(square.getX() + 300);
+            //square.setX(square.getX() + 400);
+
+            TranslateAnimation animation = new TranslateAnimation(0, 50, 0, 100);
+            animation.setDuration(1000);
+            animation.setFillAfter(false);
+            animation.setAnimationListener(new MyAnimationListener());
+
+            square.startAnimation(animation);
+
+            songID ++;
+            square.setImageDrawable(getResources().getDrawable(R.drawable.song1));
+            songName.setText(Musk._songs[songID]);
+
         }
     } else if(e1.getY() > 400){
-        if ((e1.getX() - e2.getX()) > sensitvity) {
+        if ((e1.getX() - e2.getX()) > sensitivity) {
             Toast.makeText(this, "Left to Right Swap Performed", Toast.LENGTH_LONG).show();
-            genreText.setY(genreText.getY() - 150);
-            genreText.setX(genreText.getX() - 150);
+            if(genreID == 3){
+                Toast.makeText(this, "No more playlists!", Toast.LENGTH_LONG).show();
+            } else {
+                genreID ++;
+                genreText.setText(genres[genreID]);
+            }
 
-        } else if ((e2.getX() - e1.getX()) > sensitvity) {
+        } else if ((e2.getX() - e1.getX()) > sensitivity) {
             Toast.makeText(this, "Right to Left Swap Performed", Toast.LENGTH_LONG).show();
-            genreText.setY(genreText.getY() + 150);
-            genreText.setX(genreText.getX() + 150);
+            if(genreID == 0){
+                Toast.makeText(this, "No more playlists!", Toast.LENGTH_LONG).show();
+            } else {
+                genreID --;
+                genreText.setText(genres[genreID]);
+            }
         }
 
     }
