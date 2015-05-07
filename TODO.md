d3cff19d510408061d3a5d1a1f9c43cd15dc88a7
Fred <felmqu14@student.aau.dk>
2015-05-07 11:58:40 +0200

    Beginnings of actionbar as well as updated animation (still with issues) More comments and goodies

--- TODO.md
+++ TODO.md
@@ -1,9 +1,14 @@
 **TODO:**
 
-1: Fix the transition animation of the imageView containing the pictore of the current song
+1: Fix the translation animation of the imageView containing the picture of the current song (IN PROGRESS)
 2: fix the initialization of the textView containing the song name in use
-3: Set up an array containing the images to be displayed with the songs
+~~3: Set up an array containing the images to be displayed with the songs~~
 4: Create the method to update the images accordingly
 5: Fix boundaries for the different swipes
 6: Make sure that the inner class MyAnimationListener is safe and maybe use some encapsulation
+7: Fix the myAnimationListener call for the imageView's width and height
+8: Update design so it gets the orange feels
+~~9: Create actionbar~~
+10: Customize actionbar
+
 
--- app/src/main/java/com/example/general/alternative_playlists/MainActivity.java
+++ app/src/main/java/com/example/general/alternative_playlists/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.general.alternative_playlists;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,14 +18,27 @@ import android.view.GestureDetector;
 
 public class MainActivity extends Activity implements GestureDetector.OnGestureListener{
 
+    //The gesture detector that facilitates the touchEvents
     private GestureDetectorCompat mDetector;
+
     Artist Musk = new Artist(1, "Musk", new String[]{"Fixing the server", "Cookie is dead", "No More Holt"});
+
+    //initialising the array containing images to be drawn
+    private Drawable[] drawables = null; // create a Drawables array that stores location of different images
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mDetector = new GestureDetectorCompat(this,this);
+
+        //The array containing the song images to be drawn
+        //Makes use of deprecated methods but oh well
+        drawables = new Drawable[] {
+                getResources().getDrawable(R.drawable.song),
+                getResources().getDrawable(R.drawable.song1)
+        };
     }
 
     @Override
@@ -60,6 +74,7 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
 
     }
 
+    //this method is called when a swipe is registered and contains almost all the relevant code for the app
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         final ImageView square = (ImageView)this.findViewById(R.id.imageView);
@@ -69,13 +84,17 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
         String[] genres = {"Rock","Pop","Jazz","Wub-Wub"};
         int genreID = 0;
         int songID = 0;
+        int imageID = 0;
         float sensitivity = 40;
-
+/*
+        //This inner class facilitates the animation ending as well as moving the layout
+        //of the imageView after the animation ends
         class MyAnimationListener implements Animation.AnimationListener {
 
             @Override
             public void onAnimationEnd(Animation animation) {
                 square.clearAnimation();
+                //this still calls the "square" imageView which is deprecated and needs changeing
                 RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(square.getWidth(), square.getHeight());
                 lp.setMargins(0, 300, 0, 0);
                 square.setLayoutParams(lp);
@@ -89,43 +108,56 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
             public void onAnimationStart(Animation animation) {
             }
 
-        }
+        }*/
 
-        //WORKING ON IF e1.getX() >= square.getX() && e1.getX() <= (square.getX() * 2) && e1.getY() >= square.getY() && e1.getY() <= (square.getY() * 2)
+    //Check for which element is being swiped
     if(e1.getY() <= 400) {
+        //Check for the direction of the swipe
         if ((e1.getX() - e2.getX()) > sensitivity) {
+            //Prints message on screen purely for debugging purposes
             Toast.makeText(this, "Left to Right Swap Performed", Toast.LENGTH_LONG).show();
-            //square.setX(square.getX() - 400);
-
+        /*
+            //Parameters of the translate animation
             TranslateAnimation animation = new TranslateAnimation(0, 300, 0, 0);
-            animation.setDuration(1000);
+            animation.setDuration(400);
             animation.setFillAfter(false);
             animation.setAnimationListener(new MyAnimationListener());
 
             square.startAnimation(animation);
 
+           */
+            square.animate().translationX(500).setDuration(500);
+            imageID ++;
             songID ++;
-            square.setImageDrawable(getResources().getDrawable(R.drawable.song1));
+            square.setImageDrawable(drawables[imageID]);
             songName.setText(Musk._songs[songID]);
 
         } else if ((e2.getX() - e1.getX()) > sensitivity) {
+            //Prints message on screen purely for debugging purposes
             Toast.makeText(this, "Right to Left Swap Performed", Toast.LENGTH_LONG).show();
-            //square.setX(square.getX() + 400);
-
-            TranslateAnimation animation = new TranslateAnimation(0, 50, 0, 100);
-            animation.setDuration(1000);
+            /*
+            //Parameters of the translate animation
+            TranslateAnimation animation = new TranslateAnimation(0, -300, 0, 0);
+            animation.setDuration(400);
             animation.setFillAfter(false);
             animation.setAnimationListener(new MyAnimationListener());
 
             square.startAnimation(animation);
-
+            */
+            //Simpler animation sequence. Find some way to give animations enough time to play out
+            square.animate().translationX(1000).setDuration(500);
+            square.setX(square.getX() - 1000);
+            square.setY(square.getY() - 500);
+            square.animate().translationY(500).setDuration(500);
+            imageID ++;
             songID ++;
-            square.setImageDrawable(getResources().getDrawable(R.drawable.song1));
+            square.setImageDrawable(drawables[imageID]);
             songName.setText(Musk._songs[songID]);
 
         }
     } else if(e1.getY() > 400){
         if ((e1.getX() - e2.getX()) > sensitivity) {
+            //Prints message on screen purely for debugging purposes
             Toast.makeText(this, "Left to Right Swap Performed", Toast.LENGTH_LONG).show();
             if(genreID == 3){
                 Toast.makeText(this, "No more playlists!", Toast.LENGTH_LONG).show();
@@ -135,6 +167,7 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
             }
 
         } else if ((e2.getX() - e1.getX()) > sensitivity) {
+            //Prints message on screen purely for debugging purposes
             Toast.makeText(this, "Right to Left Swap Performed", Toast.LENGTH_LONG).show();
             if(genreID == 0){
                 Toast.makeText(this, "No more playlists!", Toast.LENGTH_LONG).show();
