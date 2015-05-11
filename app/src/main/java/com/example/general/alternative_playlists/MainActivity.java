fa51c6471c51c32050245fafd8828e2325227d6f
Fred <felmqu14@student.aau.dk>
2015-05-11 12:54:03 +0200

    fixed arrays

--- app/src/main/java/com/example/general/alternative_playlists/MainActivity.java
+++ app/src/main/java/com/example/general/alternative_playlists/MainActivity.java
@@ -4,13 +4,7 @@ import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v4.view.GestureDetectorCompat;
@@ -21,17 +15,22 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
     //The gesture detector that facilitates the touchEvents
     private GestureDetectorCompat mDetector;
 
+    //The songs which were needed
+    Song song1 = new Song("Ain't No Rest for The Wicked");
+    Song song2 = new Song("Don't Stop Me Now");
+    Song song3 = new Song("");
+    Song song4 = new Song("");
+    Song song5 = new Song("");
+    Song song6 = new Song("");
+    Song song7 = new Song("");
+    Song song8 = new Song("");
+    Song song9 = new Song("");
+    Song song10 = new Song("");
+
+
     //Array
     String[] albumList = {"The 2nd Law", "Cage The Elephant"};
-    Song[] songList = new Song[10];
-
-    Song supr;
-    //Song aint = new Song("Aint No Rest for The Wicked", 1);
-    //Song dont = new Song("Don't Stop Me Now", 2);
-
-    songList[0] = new Song("Supremacy");
-
-
+    Song[] songList = {song1, song2, song3, song4, song5, song6, song7, song8, song9, song10};
 
     Artist Musk = new Artist(1, "Musk", new String[]{"Fixing the server", "Cookie is dead", "No More Holt"});
 
@@ -86,10 +85,10 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
 
     }
 
+    ImageView square = (ImageView)this.findViewById(R.id.imageView);
     //this method is called when a swipe is registered and contains almost all the relevant code for the app
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        final ImageView square = (ImageView)this.findViewById(R.id.imageView);
         //ImageView circle = (ImageView)this.findViewById(R.id.imageView2);
         TextView genreText = (TextView)this.findViewById(R.id.textView);
         TextView songName = (TextView)this.findViewById(R.id.textView2);
@@ -98,29 +97,6 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
         int songID = 0;
         int imageID = 0;
         float sensitivity = 40;
-/*
-        //This inner class facilitates the animation ending as well as moving the layout
-        //of the imageView after the animation ends
-        class MyAnimationListener implements Animation.AnimationListener {
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                square.clearAnimation();
-                //this still calls the "square" imageView which is deprecated and needs changeing
-                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(square.getWidth(), square.getHeight());
-                lp.setMargins(0, 300, 0, 0);
-                square.setLayoutParams(lp);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-        }*/
 
     //Check for which element is being swiped
     if(e1.getY() <= 400) {
@@ -128,17 +104,9 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
         if ((e1.getX() - e2.getX()) > sensitivity) {
             //Prints message on screen purely for debugging purposes
             Toast.makeText(this, "Left to Right Swap Performed", Toast.LENGTH_LONG).show();
-        /*
-            //Parameters of the translate animation
-            TranslateAnimation animation = new TranslateAnimation(0, 300, 0, 0);
-            animation.setDuration(400);
-            animation.setFillAfter(false);
-            animation.setAnimationListener(new MyAnimationListener());
 
-            square.startAnimation(animation);
-
-           */
-            square.animate().translationX(500).setDuration(500);
+            square.animate().translationX(1500).setDuration(500);
+            transIn((int) square.getX(),1000);
             imageID ++;
             songID ++;
             square.setImageDrawable(drawables[imageID]);
@@ -147,24 +115,14 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
         } else if ((e2.getX() - e1.getX()) > sensitivity) {
             //Prints message on screen purely for debugging purposes
             Toast.makeText(this, "Right to Left Swap Performed", Toast.LENGTH_LONG).show();
-            /*
-            //Parameters of the translate animation
-            TranslateAnimation animation = new TranslateAnimation(0, -300, 0, 0);
-            animation.setDuration(400);
-            animation.setFillAfter(false);
-            animation.setAnimationListener(new MyAnimationListener());
 
-            square.startAnimation(animation);
-            */
-            //Simpler animation sequence. Find some way to give animations enough time to play out
-            square.animate().translationX(1000).setDuration(500);
-            square.setX(square.getX() - 1000);
-            square.setY(square.getY() - 500);
-            square.animate().translationY(500).setDuration(500);
+
+            //Simple animation sequence
+            square.animate().translationX(1500).setDuration(500);
+            transIn((int) square.getX(),1000);
             imageID ++;
             songID ++;
-            square.setImageDrawable(drawables[imageID]);
-            songName.setText(Musk._songs[songID]);
+
 
         }
     } else if(e1.getY() > 400){
@@ -192,5 +150,13 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
     }
 
         return true;
+    }
+
+    public void transIn(int x, int x1){
+        if(x == x1){
+            square.setX(square.getX() - 1000);
+            square.setY(square.getY() - 500);
+            square.animate().translationY(500).setDuration(500);
+        }
     }
 }
